4148ed0cf975dba5cecd5496571c2a44d56a6544
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-18 19:31:23 +0000

    Options and buttons added

--- src/js/component/SecondCounter.jsx
+++ src/js/component/SecondCounter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faClock, faPlay, faStop, faClockRotateLeft, faBackwardStep, faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 
 const SecondCounter = () => {
@@ -20,16 +20,16 @@ const SecondCounter = () => {
                     setDozens(prevDozens => {
                         if (prevDozens === 9) {
                             setDozens(0);
-                            setHundreds (prevHundreds => {
+                            setHundreds(prevHundreds => {
                                 if (prevHundreds === 9) {
                                     setHundreds(0);
-                                    setThousands (prevThousands => {
+                                    setThousands(prevThousands => {
                                         if (prevThousands === 9) {
                                             setThousands(0);
-                                            setTenThousands (prevTenThousands => {
+                                            setTenThousands(prevTenThousands => {
                                                 if (prevTenThousands === 9) {
                                                     setTenThousands(0);
-                                                    setHundredThousands (prevHundredThousand => prevHundredThousand + 1);
+                                                    setHundredThousands(prevHundredThousand => prevHundredThousand + 1);
                                                 } else {
                                                     return prevTenThousands + 1;
                                                 }
@@ -50,35 +50,69 @@ const SecondCounter = () => {
                     return prevUnits + 1;
                 }
             });
+
         }, 1000);
 
         return () => clearInterval(counterInterval);
     }, []);
 
     return (
-        <div className="container-fluid d-flex align-items-center justify-content-center bg-black gap-3" style={{ height: '200px' }}>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '160px', height: '180px' }}>
-                <FontAwesomeIcon icon={faClock} spinPulse style={{ color: "#ffffff", height: '130px', width: '130px' }} />
+        <div className="row">
+            <div className="container-fluid d-flex align-items-center justify-content-center bg-black gap-3" style={{ height: '200px' }}>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '160px', height: '180px' }}>
+                    <FontAwesomeIcon icon={faClock} spinPulse style={{ color: "#ffffff", height: '130px', width: '130px' }} />
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{hundredThousands}</p>
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{tenThousands}</p>
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{thousands}</p>
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{hundreds}</p>
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{dozens}</p>
+                </div>
+                <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
+                    <p className="num-count">{units}</p>
+                </div>
             </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{hundredThousands}</p>
+            <div className="row mt-1 d-flex align-items-center justify-content-center bg-black p-3">
+                <h1 className="text-center text-white">OPCIONES</h1>
             </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{tenThousands}</p>
-            </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{thousands}</p>
-            </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{hundreds}</p>
-            </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{dozens}</p>
-            </div>
-            <div className="bg-dark rounded bg-opacity-50 color-white d-flex align-items-center justify-content-center my-shadow" style={{ width: '120px', height: '180px' }}>
-                <p className="num-count">{units}</p>
+            <div className="container-fluid d-flex align-items-center justify-content-center bg-black gap-3 p-3">
+
+                <div className="d-flex flex-column align-items-center justify-content-center border rounded-1 p-2 my-shadow">
+                    <div class="mb-1">
+                        <label for="exampleFormControlInput1" class="form-label text-white d-flex flex-column align-items-center justify-content-center">Count down</label>
+                        <input type="text" class="form-control my-custom-shadow" id="exampleFormControlInput1" placeholder="Tiempo (en segundos)" />
+                    </div>
+                    <button type="button" className="btn btn-dark mt-1 w-100"><FontAwesomeIcon icon={faClockRotateLeft} style={{ color: "#ffffff", }} />
+                    </button>
+                </div>
+                <div className="d-flex gap-1 border rounded-1 p-2 my-shadow">
+                    <button type="button" class="btn btn-dark"><FontAwesomeIcon icon={faPlay} style={{ color: "#ffffff", }} />
+                    </button>
+                    <button type="button" class="btn btn-dark"><FontAwesomeIcon icon={faStop} style={{ color: "#ffffff", }} />
+                    </button>
+                    <button type="button" class="btn btn-dark"><FontAwesomeIcon icon={faBackwardStep} style={{ color: "#ffffff", }} /></button>
+
+                </div>
+                <div className="d-flex flex-column align-items-center justify-content-center border rounded-1 p-2 my-shadow">
+                    <div class="mb-1">
+                        <label for="exampleFormControlInput1" class="form-label text-white d-flex flex-column align-items-center justify-content-center">Time for the alert</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Tiempo (en segundos)" />
+                    </div>
+                    <button type="button" className="btn btn-dark mt-1 w-100"><FontAwesomeIcon icon={faCircleExclamation} style={{ color: "#ffffff", }} />
+                    </button>
+                </div>
             </div>
         </div>
+
     )
 };
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,9 +4,8 @@ import SecondCounter from "./SecondCounter";
 //create your first component
 const Home = () => {
 	return (
-		<div className="row">
 			<SecondCounter />
-		</div>
+
 	);
 };
 
