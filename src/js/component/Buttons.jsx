29eee9342b10aaa1bf793c870f312d3bd1f979d0
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-23 18:24:03 +0000

    some styles added

--- src/js/component/Buttons.jsx
+++ src/js/component/Buttons.jsx
@@ -5,27 +5,27 @@ import { faPlay, faPause, faBackwardStep } from '@fortawesome/free-solid-svg-ico
 const Buttons = ({ handleReset, handleStop, handleResume }) => {
     return (
         <div className="row">
-            <div className="container-fluid d-flex align-items-center justify-content-center bg-black gap-3">
-                <h1 className="text-center text-white">OPCIONES</h1>
+            <div className="container-fluid d-flex align-items-center justify-content-center bg-black">
+                <h1 className="text-center text-white rounded border mt-2 my-shadow p-1">OPCIONES</h1>
             </div>
             <div className="container-fluid d-flex align-items-center justify-content-center bg-black gap-3 p-3">
                 <button
                     id="btn-reset"
-                    className="btn btn-secondary mb-2 me-2 border border-light"
+                    className="btn btn-dark mb-2 me-2 border border-light"
                     onClick={handleReset}
                 >
                     <FontAwesomeIcon icon={faBackwardStep} style={{ color: "#ffffff", }} />
                 </button>
                 <button
                     id="btn-stop"
-                    className="btn btn-secondary mb-2 me-2 border border-light"
+                    className="btn btn-dark mb-2 me-2 border border-light"
                     onClick={handleStop}
                 >
                     <FontAwesomeIcon icon={faPause} style={{ color: "#ffffff", }} />
                 </button>
                 <button
                     id="btn-resume"
-                    className="btn btn-secondary mb-2 border border-light"
+                    className="btn btn-dark mb-2 border border-light"
                     onClick={handleResume}
                 >
                     <FontAwesomeIcon icon={faPlay} style={{ color: "#ffffff", }} />
